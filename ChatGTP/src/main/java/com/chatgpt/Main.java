1bb5873e3d71eaee63fb8c0ff225c1ebb4225b0b
Matteus Moreno <40833478+matteusmoreno@users.noreply.github.com>
2023-08-02 13:57:55 -0300

    Add files via upload

---
+++ ChatGTP/src/main/java/com/chatgpt/Main.java
@@ -0,0 +1,27 @@
+package com.chatgpt;
+
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.service.OpenAiService;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        OpenAiService service = new OpenAiService("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");
+
+        var leitura = new Scanner(System.in);
+        System.out.println("Digite: ");
+        var pergunta = leitura.nextLine();
+
+        CompletionRequest request = CompletionRequest.builder()
+                .model("text-davinci-003")
+                .prompt(pergunta)
+                .maxTokens(2800)
+                .build();
+
+        var resposta = service.createCompletion(request);
+
+        System.out.println(resposta.getChoices().get(0).getText());
+    }
+}
